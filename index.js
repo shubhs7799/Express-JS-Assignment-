3451f4a91ea0a5ba738e85e537d4b448ae25aef2
Your Name <rshubham928@gmail.com>
2025-06-06 13:46:55 +0530

    Middleware for logging

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const app = express();
 
-// Dynamic route using route param (:username) and query param (?role=...)
-app.get('/welcome/:username', (req, res) => {
-  const { username } = req.params;       // Route parameter
-  const { role } = req.query;            // Query parameter
-
-  if (!role) {
-    return res.send(`Welcome ${username}, please provide your role using "?role=YourRole"`);
-  }
-
-  res.send(`Welcome ${username}, your role is ${role}`);
+// === Logging Middleware ===
+app.use((req, res, next) => {
+  console.log(`${req.method} request made to ${req.url}`);
+  next();
 });
 
-// Start server
+// === Products Routes ===
+app.get('/products', (req, res) => {
+  res.send('Here is the list of all products.');
+});
+
+app.post('/products', (req, res) => {
+  res.send('A new product has been added.');
+});
+
+// === Categories Routes ===
+app.get('/categories', (req, res) => {
+  res.send('Here is the list of all categories.');
+});
+
+app.post('/categories', (req, res) => {
+  res.send('A new category has been created.');
+});
+
+// === Wildcard Route for 404 ===
+app.all('/:any', (req, res) => {
+  res.status(404).send('<h1>404 - Page Not Found</h1>');
+});
+
+// === Start Server ===
 app.listen(4000, () => {
-  console.log('Server running at http://localhost:4000');
+  console.log('Server is running on http://localhost:4000');
 });
