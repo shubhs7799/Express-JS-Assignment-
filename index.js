cd677bd74a64901877a4e7abcc92e10c129fbf44
Your Name <rshubham928@gmail.com>
2025-06-06 14:09:22 +0530

    Router and filter task

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const app = express();
-const apiRoutes = require('./routes/apiRoutes'); // import router
+const booksRouter = require('./routes/books');
 
-// Logging middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} request made to ${req.url}`);
-  next();
-});
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-// Mount all /products and /categories routes
-app.use('/', apiRoutes);
+// Use the books router
+app.use('/books', booksRouter);
 
-// 404 Wildcard Route
-app.all('/:any', (req, res) => {
-  res.status(404).send('<h1>404 - Page Not Found</h1>');
-});
-
-// Start server
+// Start the server
 app.listen(4000, () => {
   console.log('Server is running on http://localhost:4000');
 });
